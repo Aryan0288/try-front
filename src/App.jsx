4f0138b481a47d04a1d88f3633c96005afced9dd
Aryan <aryansharma31658@gmail.com>
2023-12-26 06:49:46 +0530

    port no

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  axios.defaults.baseURL='http://localhost:4040';
-  axios.defaults.baseURL='https://chat-back-r65u.onrender.com';
+  // axios.defaults.baseURL='http://localhost:4040';
+  axios.defaults.baseURL="https://chat-back-r65u.onrender.com"
+  // axios.defaults.baseURL = process.env.NODE_ENV === 'development'
+  // ? 'http://localhost:4040'
+  // : 'https://chat-back-r65u.onrender.com';
+  
   axios.defaults.withCredentials=true;
 
   return (
