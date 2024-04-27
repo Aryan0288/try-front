901f4a69a9f37c4f60854f7870538f24fcf29b66
Aryan <aryansharma31658@gmail.com>
2024-04-27 23:31:02 +0530

    css

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -85,6 +85,12 @@ export default function Chat() {
     async function sendMessage(ev, file = null) {
 
         if (ev) ev.preventDefault();
+        if (newMessageText.trim().length == 0) {
+            toast.warning("Can't send empty message", {
+                position: "bottom-center"
+            });
+            return;
+        }
         console.log("selected user id : ", selectedUserId);
         console.log("newMessageText : ", newMessageText);
         ws.send(JSON.stringify({
@@ -227,6 +233,10 @@ export default function Chat() {
         // For simplicity, we're just logging messages to the console.
     };
 
+    function backClick() {
+        setSelectedUserId(null);
+    }
+
     return (
         // <div className='md:h-screen h-screen flex w-screen'>
         //     <div className="bg-blue-200 w-1/3 flex flex-col justify-between">
@@ -262,8 +272,10 @@ export default function Chat() {
                         ))
                     }
                 </div>
-                <div className='flex md:flex-row flex-col items-center justify-center lg:p-2'>
-                    <div className='flex  items-center gap-1 cursor-pointer mr-2 capitalize font-mono text-2xl font-bold'>
+                <div className='flex flex-row items-center justify-center lg:p-2'>
+                    <div className='flex items-center gap-1 cursor-pointer mr-2 capitalize font-mono text-2xl font-bold'>
+
+
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -274,7 +286,7 @@ export default function Chat() {
                         </div>
                     </div>
                     <div onClick={logout} className='flex bg-yellow-500 items-center justify-center'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 p-2 cursor-pointer bg-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 p-2 cursor-pointer">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
                         </svg>
                         <button title='SignOut' className='text-md text-black font-bold lg:p-2'>Sign out &nbsp;</button>
@@ -298,7 +310,16 @@ export default function Chat() {
 
                     {!!selectedUserId && (
                         <div className='relative h-full max-md:w-screen'>
-                            <div className='overflow-y-scroll absolute inset-0 ml-4 pr-5'>
+                            <div className='overflow-y-scroll absolute inset-0 ml-4 pt-12 pr-5'>
+
+                                <div className='items-center flex justify-between  bg-blue-800 fixed top-0 left-0 right-0 bottom-0 h-10'>
+                                    <button onClick={backClick} className=" font-mono font-semibold text-white text-left pl-4 ">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                        </svg>
+                                    </button>
+                                    <p className='font-mono font-semibold text-white pr-3 text-xl capitalize '>{username}</p>
+                                </div>
 
                                 {messagesWithoutDupes.map(message => (
                                     <div key={message._id} className={(message.sender === id ? 'text-right' : 'text-left')}>
