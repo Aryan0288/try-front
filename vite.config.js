e3746a9952b6b5f02d29a2d1c3276f73fcc53cc5
Aryan <aryansharma31658@gmail.com>
2024-04-27 16:37:51 +0530

    paryas

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//   ],
-//   define:{
-//     'process.env.REACT_APP_BASE_URL':JSON.stringify(process.env.REACT_APP_BASE_URL),
-//   }
-// })
-
-
-import { defineConfig } from "vite";
-import { resolve } from "path";
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        signUp: resolve(__dirname, "src/SignUp.js"), // Path to your SignUp component
-        loginPage: resolve(__dirname, "src/LoginPage.js"), // Path to your LoginPage component
-        chat: resolve(__dirname, "src/Chat.js"), // Path to your Chat component
-        otpVerification: resolve(__dirname, "src/OTPVerification.js"), // Path to your OTPVerification component
-        // Add more entry points for other components as needed
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+  ]
+})
+
+
+// import { defineConfig } from "vite";
+// import { resolve } from "path";
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         signUp: resolve(__dirname, "src/SignUp.jsx"), // Path to your SignUp component
+//         loginPage: resolve(__dirname, "src/LoginPage.jsx"), // Path to your LoginPage component
+//         chat: resolve(__dirname, "src/Chat.jsx"), // Path to your Chat component
+//         otpVerification: resolve(__dirname, "src/Components/OTPVerification.jsx"), // Path to your OTPVerification component
+//         // Add more entry points for other components as needed
+//       },
+//     },
+//   },
+// });
